74ca5dcf38e6b1c8b084bacee7dae3c8b130ea4e
Aman Danidhariya <amandanidhariya007@gmail.com>
2024-06-18 21:43:56 +0530

    finish the App

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 const Results = ({ input }) => {
-  const results=calculateInvestmentResults(input);
+  const results = calculateInvestmentResults(input);
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
 
-  console.log(results);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((tableItem) => {
+          const totalInterest =
+            tableItem.valueEndOfYear -
+            tableItem.annualInvestment * tableItem.year -
+            initialInvestment;
 
-  return <table>Results...</table>;
+          const totalAmountInvested = tableItem.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={tableItem.year}>
+              <td>{tableItem.year}</td>
+              <td>{formatter.format(tableItem.valueEndOfYear)}</td>
+              <td>{formatter.format(tableItem.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Results;
